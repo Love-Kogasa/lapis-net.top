b294ab81fc4a1758659b69e1fc25abe436e9ee1c
Love-Kogasa <dachao_88@qq.com>
2026-02-06 23:08:45 +0800

    fix url

--- src/libs/fetch-data.js
+++ src/libs/fetch-data.js
@@ -1,7 +1,7 @@
 import json5 from 'json5';
 
 const urlBase =
-'https://ghget.xsawa.dpdns.org/https://raw.githubusercontent.com/LapisNet/.public_data/refs/heads/main/';
+'https://cdn.jsdelivr.net/gh/LapisNet/.public_data/';
 
 const dataNames = {
 	news: 'news.txt',
@@ -20,8 +20,6 @@ const dataNames = {
 export const fetchData = async(name) => {
 	if(name === 'news' || name === 'projects') console.warn(`[Deprecation Warning] Fetching "${name}" from fetchData is deprecated and will be removed in future versions. Please migrate to the new data source.`);
 	if(!dataNames[name]) throw new Error(`Data "${name}" is not defined in fetchData.`);
-	const res = await fetch(urlBase + dataNames[name], {
-		headers: {'LateDream-Authorization': 'Late 520dRe@m'}
-	});
+	const res = await fetch(urlBase + dataNames[name]);
 	return name === 'news'? await res.text(): json5.parse(await res.text());
 }
